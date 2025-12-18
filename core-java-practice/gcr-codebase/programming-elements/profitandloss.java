8e77dfe3eae6d295fc7eaa686affd339ada13ef2
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-18 11:35:05 +0530

    [samradh] Add.Implement profit and loss calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/profitandloss.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class profitandloss {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter cost price in INR: ");
+        int costpr = sc.nextInt();
+        System.out.print("Enter selling price in INR: ");
+        int sellpr = sc.nextInt();
+        System.out.printf("The Cost Price int INR %d and Selling Price in INR %d%nThe Profit is INR %d and the Profit Percentage is %.2f", costpr, sellpr, (sellpr - costpr), (double) (sellpr - costpr) / costpr * 100);
+    }
+}
