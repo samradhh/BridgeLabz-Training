5e16f98d9e946f5b72273a004c1e33e3cb3cab6e
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-18 10:35:22 +0530

    [samradh] Add.Implement area calculation for a circle

---
+++ core-java-practice/gcr-codebase/programming-elements/areaofcir.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class areaofcir {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius of circle: ");
+        int rad = sc.nextInt();
+        double ar = Math.PI * rad * rad;
+        System.out.println("Area of circle: " + ar);
+        sc.close();
+    }
+}
