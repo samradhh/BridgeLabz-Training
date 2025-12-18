a1b0dc413c2300777ff1ca5b40d025387ea7cc71
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-18 10:42:13 +0530

    [samradh] Add.Implement volume calculation for a cylinder

---
+++ core-java-practice/gcr-codebase/programming-elements/volume.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class volume {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius: ");
+        int radi = sc.nextInt();
+        System.out.println("Enter height: ");
+        int heigh = sc.nextInt();
+        double volu = Math.PI * Math.pow(radi, 2) * heigh;
+        System.out.println("Volume of cylinder: " + volu);
+        sc.close();
+    }
+}
