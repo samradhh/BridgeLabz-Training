db810b59401857c472bfcd0b94e7183a884043f3
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-18 15:03:28 +0530

    [samradh] Add.height conversion program from cm to feet and inches

---
+++ core-java-practice/gcr-codebase/programming-elements/inches.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class inches {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height in cm = ");
+        double h = sc.nextDouble();
+        double cmpinch = 2.54;
+        int inchpf = 12;
+        int totalInches = (int) Math.round(h / cmpinch);
+        int fe = totalInches / inchpf;
+        int inch = totalInches % inchpf;
+        System.out.println("Your Height in cm is " + h + " while in feet is " + fe + " and inches is " + inch);
+        sc.close();
+    }
+}
