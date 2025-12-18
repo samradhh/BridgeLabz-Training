f26a594e91f53c34df012f4a2ca904030abfc072
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-18 12:20:18 +0530

    [samradh] Add.earth volume calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/earthvolume.java
@@ -0,0 +1,6 @@
+public class earthvolume {
+    public static void main(String[] args) {
+        int radius = 6378; // in kilometers
+        System.out.printf("the volume earth in cubic kilometers is %.2f and cumbic meters is %.2f",(double)(4/3)*Math.PI*Math.pow(radius, 3),(double)(4/3)*Math.PI*Math.pow(radius*1000, 3));
+    }
+}
