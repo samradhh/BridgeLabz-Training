008cd029b63ee14f1bb20eb71aa3299e2152f412
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-19 11:19:21 +0530

    [samradh] Refactor.program to calculate the volume of the Earth.

--- core-java-practice/gcr-codebase/programming-elements/VolumeEarth.java
+++ core-java-practice/gcr-codebase/programming-elements/VolumeEarth.java
@@ -1,4 +1,4 @@
-public class earthvolume {
+public class VolumeEarth {
     public static void main(String[] args) {
         int radius = 6378; // in kilometers
         System.out.printf("the volume earth in cubic kilometers is %.2f and cumbic meters is %.2f",(double)(4/3)*Math.PI*Math.pow(radius, 3),(double)(4/3)*Math.PI*Math.pow(radius*1000, 3));
