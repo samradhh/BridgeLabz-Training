f811388e5f99b2d67c489796559d9f11cbfb0241
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-19 11:09:02 +0530

    [samradh] Refactor.program to calculate the length of a side from the perimeter of a square

---
+++ core-java-practice/gcr-codebase/programming-elements/LengthCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LengthCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the perimeter of the square = ");
+        double per = sc.nextDouble();
+        double sd = per / 4.0;
+        System.out.println("The length of the side is " + sd + " whose perimeter is " + per);
+        sc.close();
+    }
+}
