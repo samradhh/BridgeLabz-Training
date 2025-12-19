e2d79e663fdd14d65cea5acd47727565d69ddb65
Samradh Srivastava <121005056+samradhh@users.noreply.github.com>
2025-12-19 11:36:58 +0530

    [samradh] Refactor.Update and rename temperatureconversion.java to TemperatureConversion.java

--- core-java-practice/gcr-codebase/programming-elements/TemperatureConversion.java
+++ core-java-practice/gcr-codebase/programming-elements/TemperatureConversion.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-public class temperatureconversion {
+public class TemperatureConversion {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
