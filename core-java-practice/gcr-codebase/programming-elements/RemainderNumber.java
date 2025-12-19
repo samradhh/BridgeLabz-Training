e36f1bb7861e965ff9237bfa1d0388d7f3e38393
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-19 11:13:47 +0530

    [samradh] Refactor.program to calculate the quotient and remainder of two numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/RemainderNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RemainderNumber {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number = ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter second number = ");
+        int n2 = sc.nextInt();
+        System.out.printf("The Quotient is %d and Reminder is %d of two number %d and %d", n1 / n2, n1 % n2, n1, n2);
+        sc.close();
+    }
+}
