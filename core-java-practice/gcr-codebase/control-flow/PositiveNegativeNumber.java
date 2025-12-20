b887cf9906786334c8e9f38c2cb71de41a6729dd
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-20 12:41:46 +0530

    [samradh] Add.program to check weather the number is positive or negative

---
+++ core-java-practice/gcr-codebase/control-flow/PositiveNegativeNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = sc.nextInt();
+		System.out.printf("If the number is positive, print %s",
+				number <= 0 ? number == 0 ? "Zero" : "Negative" : "Positive");
+		sc.close();
+	}
+}
