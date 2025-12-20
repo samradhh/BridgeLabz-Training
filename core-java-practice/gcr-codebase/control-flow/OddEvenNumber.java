5706655c6ec4befc51db31e26a6573689e86f383
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-20 12:40:23 +0530

    [samradh] Add.program to check for odd or even number

---
+++ core-java-practice/gcr-codebase/control-flow/OddEvenNumber.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class OddEvenNumber 
+{
+	public static void main(String[] args) 
+    {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number = ");
+		int input = sc.nextInt();
+		if (input < 0) 
+        {
+			System.out.print("Number is not natural");
+		} 
+        else
+         {
+			for (int i = 1; i <= input; i++)
+             {
+				System.out.printf("%d is %s\n", i, i % 2 == 0 ? "Even" : "Odd");
+			}
+		}
+        
+		sc.close();
+	}
+}
