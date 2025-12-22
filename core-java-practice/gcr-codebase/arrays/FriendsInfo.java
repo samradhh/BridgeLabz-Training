0df458e551ad3f782b7b992bb8e3a233c4d578e9
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-22 23:27:12 +0530

    [samradh] Add.program for friends information

---
+++ core-java-practice/gcr-codebase/arrays/FriendsInfo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class FriendsInfo 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        for (int i = 0; i < names.length; i++) 
+        {
+            age[i] = sc.nextInt();
+            height[i] = sc.nextDouble();
+        }
+
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        for (int i = 1; i < names.length; i++) 
+        {
+            if (age[i] < age[youngestIndex]) 
+            {
+                youngestIndex = i;
+            }
+            if (height[i] > height[tallestIndex])
+             {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("Youngest friend is " + names[youngestIndex]);
+        System.out.println("Tallest friend is " + names[tallestIndex]);
+
+        sc.close();
+    }
+}
