6fe128587248dd894ed6b8378ca4acc623e31f22
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-23 10:23:29 +0530

    [samradh] Add.program to find positive negative numbers

--- core-java-practice/gcr-codebase/control-flow/PositiveNegativeNumbers.java
+++ core-java-practice/gcr-codebase/control-flow/PositiveNegativeNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PositiveNegativeNumber {
+public class PositiveNegativeNumbers {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a number: ");
