f6780bacb16527582722a5a0021ca2aa4f48f111
Samradh Srivastava <samradhsrivastava@gmail.com>
2025-12-29 15:16:27 +0530

    [samradh] Add.program to generate random heights for a football team and calculate their sum, mean, shortest, and tallest heights

---
+++ core-java-practice/gcr-codebase/methods/FootballTeamHieghts.java
@@ -0,0 +1,55 @@
+public class FootballTeamHieghts {
+
+    public static int[] generateHeights() {
+        int[] heights = new int[11];
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = (int) (Math.random() * 101) + 150;
+        }
+        return heights;
+    }
+
+    public static int findSum(int[] heights) {
+        int sum = 0;
+        for (int i = 0; i < heights.length; i++) {
+            sum += heights[i];
+        }
+        return sum;
+    }
+
+    public static double findMean(int[] heights) {
+        int sum = findSum(heights);
+        return (double) sum / heights.length;
+    }
+
+    public static int findShortest(int[] heights) {
+        int min = heights[0];
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] < min) {
+                min = heights[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findTallest(int[] heights) {
+        int max = heights[0];
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > max) {
+                max = heights[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = generateHeights();
+
+        for (int i = 0; i < heights.length; i++) {
+            System.out.println(heights[i]);
+        }
+
+        System.out.println("Shortest Height: " + findShortest(heights));
+        System.out.println("Tallest Height: " + findTallest(heights));
+        System.out.println("Mean Height: " + findMean(heights));
+    }
+}
