db013fe1a59cb9205a3bf3fbd6bb164f0d75a18a
Samradh Srivastava <samradhsrivastava@gmail.com>
2026-01-06 16:01:09 +0530

    [samradh] Add.TwoSum class to implement functionality for finding two numbers that add up to a specific target, enhancing problem-solving capabilities in array manipulation.

---
+++ core-java-practice/leet-code-codebase/TwoSum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[][] arr = new int[nums.length][2];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int sum = arr[left][0] + arr[right][0];
+
+            if (sum == target) {
+                return new int[] { arr[left][1], arr[right][1] };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = r.nextInt();
+        }
+        int k = r.nextInt();
+        int b[] = twoSum(a, k);
+        System.out.print(Arrays.toString(b));
+        r.close();
+    }
+}
