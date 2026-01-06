20d421118a693448ed1f4eb45cd8d7acc719d76a
Samradh Srivastava <samradhsrivastava@gmail.com>
2026-01-06 16:00:47 +0530

    [samradh] Add.RotateArray class to implement functionality for rotating an array, enhancing array manipulation capabilities.

---
+++ core-java-practice/leet-code-codebase/RotateArray.java
@@ -0,0 +1,14 @@
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+    private void reverse(int[] a, int l, int r) {
+        while (l<r) {
+            int t = a[l]; a[l++] = a[r]; a[r--] = t;
+        }
+    }
+}
